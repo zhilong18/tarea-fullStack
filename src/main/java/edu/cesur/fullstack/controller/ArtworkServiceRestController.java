91d555e7631c0a9049fa3784fd9495a921a2b855
zhilong18 <126709677+zhilong18@users.noreply.github.com>
2023-11-08 21:07:17 +0100

    terminado de verdad

--- src/main/java/edu/cesur/fullstack/controller/ArtworkServiceRestController.java
+++ src/main/java/edu/cesur/fullstack/controller/ArtworkServiceRestController.java
@@ -1,5 +1,7 @@
 package edu.cesur.fullstack.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import edu.cesur.fullstack.model.ArtworkDTO;
 import edu.cesur.fullstack.services.ArtworkService;
 
 @RestController
@@ -26,6 +30,21 @@ public class ArtworkServiceRestController {
 	public ResponseEntity<?> getObras(@PathVariable Integer artistId) {
 
 		return ResponseEntity.ok(artworkService.getAllArtworks(artistId));
+	}
+	
+	@PostMapping()
+	public ResponseEntity<?> reserveBook(@PathVariable ArtworkDTO artworkDTO){
+		
+    	artworkService.createArtwork(artworkDTO);
+		
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(artworkDTO.getId())
+				.toUri();
+		
+		return ResponseEntity.created(location).build();		
+		
 	}
 	
 }
--- src/main/java/edu/cesur/fullstack/exeptions/handlers/GlobalExeptionHandler.java
+++ src/main/java/edu/cesur/fullstack/exeptions/handlers/GlobalExeptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import edu.cesur.fullstack.exeptions.ArtworkCreationException;
 import edu.cesur.fullstack.exeptions.artistNotFoundException;
 
 @RestControllerAdvice
@@ -20,6 +21,18 @@ public class GlobalExeptionHandler {
 		problemDetail.setTitle("Artwork Exception Occurred");
 		problemDetail.setType(URI.create("http://cesurformacion.com"));
 		problemDetail.setProperty("errorCategory", "Artwork");
+		problemDetail.setProperty("timeStamp", Instant.now());
+
+		return problemDetail;
+	}
+	
+	@ExceptionHandler( ArtworkCreationException.class )
+	public ProblemDetail handleArtworkCreationException(ArtworkCreationException e) {
+
+		ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+		problemDetail.setTitle("CrearArtwork Exception Occurred");
+		problemDetail.setType(URI.create("http://cesurformacion.com"));
+		problemDetail.setProperty("errorCategory", "CrearArtwork");
 		problemDetail.setProperty("timeStamp", Instant.now());
 
 		return problemDetail;
